eeb2014d6caea31ab13298b1d76e015bfef27eff
BaoAu <doraemonagb@gmail.com>
2025-02-15 11:25:58 +0700

    3d pin cards added to project sec

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,12 +1,38 @@
+import { projects } from '@/data'
 import React from 'react'
+import { PinContainer } from './ui/3d-pins'
 
 const RecentProjects = () => {
   return (
     <div className='py-20'>
         <h1 className='heading'>
             A small selection of {' '}
-            <span>my recent projects</span>
+            <span className='text-purple'>my recent projects</span>
         </h1>
+
+        <div className='flex flex-wrap items-center justify-center p-4 gap-16 mt-10'>
+          {projects.map(( {id, title, des, img, iconLists, link} ) => (
+            <div key={id} className='lg:min-h-[32.5rem] h-[25rem] w-[80vw] flex items-center justify-center sm:w-96'>
+              <PinContainer
+                title={title} 
+                href={link}
+              >
+                <div className='relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10'>
+                  <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]'>
+                    <img src='/bg.png' alt='bg-img' />
+                  </div>
+                  <img src={img} alt={title} className='z-10 absolute bottom-0'/>
+                </div>
+                <h1 className='font-bold lg:text-2xl md:text-xl text-base line-clamp-1'>
+                  {title}
+                </h1>
+                <p className='lg:text-xl lg:font-normal font-light text-sm line-clamp-2'>
+                  {des}
+                </p>
+              </PinContainer>
+            </div>
+          ))}
+        </div>
     </div>
   )
 }
